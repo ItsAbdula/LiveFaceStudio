f5c99bbdef663e6b0e4d09eb64387b4fb4f07d23
ItsAbdula <abdula2523@ajou.ac.kr>
2020-03-14 11:46:34 +0900

    전처리 구문 추가. 에디터에서 x86, x64용 dll을 찾을 수 없어서 에러나는 상황.

--- MPUnity/Assets/Scripts/CallNativeCode.cs
+++ MPUnity/Assets/Scripts/CallNativeCode.cs
@@ -4,12 +4,18 @@ using System.Runtime.InteropServices;
 
 public class CallNativeCode : MonoBehaviour
 {
+#if !UNITY_EDITOR_WIN
+
     [DllImport("OpenCV_Android")]
     private static extern float Foopluginmethod();
+
+#endif
 
     private void OnGUI()
     {
         // This Line should display "Foopluginmethod: 10"
+#if !UNITY_EDITOR_WIN
         GUI.Label(new Rect(15, 125, 450, 100), "Foopluginmethod: " + Foopluginmethod());
+#endif
     }
 }
