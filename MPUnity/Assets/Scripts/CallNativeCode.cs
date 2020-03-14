314c9915bd52ccfbb8f788d7cb96944d28ab4f61
ItsAbdula <abdula2523@ajou.ac.kr>
2020-03-14 11:29:53 +0900

    C#에서 OpenCV 테스트용 코드 추가

---
+++ MPUnity/Assets/Scripts/CallNativeCode.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class CallNativeCode : MonoBehaviour
+{
+    [DllImport("OpenCV_Android")]
+    private static extern float Foopluginmethod();
+
+    private void OnGUI()
+    {
+        // This Line should display "Foopluginmethod: 10"
+        GUI.Label(new Rect(15, 125, 450, 100), "Foopluginmethod: " + Foopluginmethod());
+    }
+}
