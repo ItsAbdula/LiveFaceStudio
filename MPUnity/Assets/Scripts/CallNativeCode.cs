11cf5d7aeabc8692c52c8158fb275e6656640a6c
ItsAbdula <abdula2523@ajou.ac.kr>
2020-03-15 12:14:09 +0900

    유니티에서 dll불러오는 코드 수정.

--- MPUnity/Assets/Scripts/CallNativeCode.cs
+++ MPUnity/Assets/Scripts/CallNativeCode.cs
@@ -4,18 +4,20 @@ using System.Runtime.InteropServices;
 
 public class CallNativeCode : MonoBehaviour
 {
-#if !UNITY_EDITOR_WIN
-
-    [DllImport("OpenCV_Android")]
-    private static extern float Foopluginmethod();
-
+#if UNITY_EDITOR_WIN
+    private const string dllName = "OpenCV_Windows";
+#elif UNITY_ANDROID
+    private const string dllName = "OpenCV_Android";
+#else
+    private const string dllName = "_";
 #endif
+
+    [DllImport(dllName)]
+    private static extern float Foopluginmethod();
 
     private void OnGUI()
     {
         // This Line should display "Foopluginmethod: 10"
-#if !UNITY_EDITOR_WIN
         GUI.Label(new Rect(15, 125, 450, 100), "Foopluginmethod: " + Foopluginmethod());
-#endif
     }
 }
