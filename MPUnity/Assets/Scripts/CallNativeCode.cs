3b7c583fd263ebac0660002519613ef5f25059de
ItsAbdula <abdula2523@ajou.ac.kr>
2020-03-25 05:55:01 +0900

    CallNativeCode static으로 변경. 같이 붙어있는 UI코드들 제거.

--- MPUnity/Assets/Scripts/CallNativeCode.cs
+++ MPUnity/Assets/Scripts/CallNativeCode.cs
@@ -1,11 +1,10 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 public class CallNativeCode : MonoBehaviour
 {
+    public static CallNativeCode callNativeCode;
+
 #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
     private const string dllName = "OpenCV_Windows";
 #elif UNITY_ANDROID
@@ -14,58 +13,12 @@ public class CallNativeCode : MonoBehaviour
     private const string dllName = "_";
 #endif
 
-    public Button btnFlipImage;
-    public Button btnDetectImage;
-    public Image originalImage;
-
-    public RawImage detectedImage;
+    [DllImport(dllName)]
+    public static extern void FlipImage(ref Color32[] rawImage, int width, int height);
 
     [DllImport(dllName)]
-    private static extern void FlipImage(ref Color32[] rawImage, int width, int height);
+    public static extern void DetectFace(ref Color32[] rawImage, int width, int height);
 
     [DllImport(dllName)]
-    private static extern void DetectFace(ref Color32[] rawImage, int width, int height);
-
-    [DllImport(dllName)]
-    private static extern float Foopluginmethod();
-
-    private void Start()
-    {
-        // btnFlipImage.onClick.AddListener(CallFlipImage);
-        // btnDetectImage.onClick.AddListener(CallDetectFace);
-    }
-
-    private float timer = 0;
-
-    private void Update()
-    {
-        timer += Time.deltaTime;
-
-        if (timer > 0.05f)
-        {
-            timer = 0;
-
-            if (WebCam.canUseDevices == false) return;
-
-            detectedImage.texture = GetDetectedTexture();
-        }
-    }
-
-    private Texture2D GetDetectedTexture()
-    {
-        DetectFace(ref WebCam.image, WebCam.Width, WebCam.Height);
-
-        Texture2D newTexture = new Texture2D(WebCam.Width, WebCam.Height, TextureFormat.RGB24, false, false);
-        newTexture.SetPixels32(WebCam.image, 0);
-        newTexture.Apply();
-        detectedImage.texture = newTexture;
-
-        return newTexture;
-    }
-
-    private void OnGUI()
-    {
-        // This Line should display "Foopluginmethod: 10"
-        GUI.Label(new Rect(15, 125, 450, 100), "Foopluginmethod: " + Foopluginmethod());
-    }
+    public static extern float Foopluginmethod();
 }
