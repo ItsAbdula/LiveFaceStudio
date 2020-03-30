9829e77f42290d927d9981e8a51a131d4b0273e8
ItsAbdula <abdula2523@ajou.ac.kr>
2020-03-30 09:04:33 +0900

    OpenCV_Windows 기존 코드 정리

--- OpenCV_Windows/Source.cpp
+++ OpenCV_Windows/Source.cpp
@@ -10,73 +10,28 @@ using namespace std;
 using namespace cv;
 using namespace face;
 
-void detectAndDraw(Mat& img, CascadeClassifier& cascade,
-    CascadeClassifier& nestedCascade,
-    double scale, bool tryflip);
+void detectAndDraw(Mat& img, CascadeClassifier& cascade, CascadeClassifier& nestedCascade, double scale);
 
 void detectFaceLandmark(string fileName, string image, string cascade_name);
 
-string cascadeName;
-string nestedCascadeName;
-
 int main(int argc, const char** argv)
 {
-    /* VideoCapture capture;
-     Mat frame, image;
-     string inputName;
-     bool tryflip;
-     CascadeClassifier cascade, nestedCascade;
-     double scale;
-     cv::CommandLineParser parser(argc, argv,
-         "{help h||}"
-         "{cascade|data/haarcascades/haarcascade_frontalface_alt.xml|}"
-         "{nested-cascade|data/haarcascades/haarcascade_eye_tree_eyeglasses.xml|}"
-         "{scale|1|}{try-flip||}{@filename|test.jpg|}"
-     );
-     cascadeName = parser.get<string>("cascade");
-     nestedCascadeName = parser.get<string>("nested-cascade");
+    Mat image;
+    string inputName = "test.jpg";
+    bool tryflip = false;
+    CascadeClassifier cascade, nestedCascade;
+    cascade.load("data/haarcascades/haarcascade_frontalface_alt.xml");
+    nestedCascade.load("data/haarcascades/haarcascade_eye_tree_eyeglasses.xml");
 
-     scale = parser.get<double>("scale");
-     scale = std::max(1.0, scale);
+    image = imread(samples::findFileOrKeep(inputName), IMREAD_COLOR);
 
-     tryflip = parser.has("try-flip");
-     inputName = parser.get<string>("@filename");
-     if (parser.check() == false)
-     {
-         parser.printErrors();
-         return 0;
-     }
-     if (nestedCascade.load(samples::findFileOrKeep(nestedCascadeName)) == false)
-         cerr << "WARNING: Could not load classifier cascade for nested objects" << endl;
-     if (cascade.load(samples::findFile(cascadeName)) == false)
-     {
-         cerr << "ERROR: Could not load classifier cascade" << endl;
-         return -1;
-     }
-     if (inputName.empty() || (isdigit(inputName[0]) && inputName.size() == 1))
-     {
-         cout << "Not Input Name!" << endl;
-         return 1;
-     }
-     else if (inputName.empty() == false)
-     {
-         image = imread(samples::findFileOrKeep(inputName), IMREAD_COLOR);
-         if (image.empty())
-         {
-             if (!capture.open(samples::findFileOrKeep(inputName)))
-             {
-                 cout << "Could not read " << inputName << endl;
-                 return 1;
-             }
-         }
-     }
+    if (image.empty() == false)
+    {
+        if (tryflip) flip(image, image, 1);
 
-     cout << "Detecting face(s) in " << inputName << endl;
-     if (image.empty() == false)
-     {
-         detectAndDraw(image, cascade, nestedCascade, scale, tryflip);
-         waitKey(0);
-     }*/
+        detectAndDraw(image, cascade, nestedCascade, 1.0);
+        waitKey(0);
+    }
 
     detectFaceLandmark("data/face/face_landmark_model.dat", "george.jpg", "data/haarcascades/haarcascade_frontalface_alt.xml");
 
@@ -132,90 +87,55 @@ void detectFaceLandmark(string modelDataName, string image, string cascade_name)
     }
 }
 
-void detectAndDraw(Mat& img, CascadeClassifier& cascade,
-    CascadeClassifier& nestedCascade,
-    double scale, bool tryflip)
+void detectAndDraw(Mat& img, CascadeClassifier& cascade, CascadeClassifier& nestedCascade, double scale)
 {
-    double t = 0;
-    vector<Rect> faces, faces2;
-    const static Scalar colors[] =
-    {
-            Scalar(255,0,0),
-            Scalar(255,128,0),
-            Scalar(255,255,0),
-            Scalar(0,255,0),
-            Scalar(0,128,255),
-            Scalar(0,255,255),
-            Scalar(0,0,255),
-            Scalar(255,0,255)
-    };
-    Mat gray, smallImg;
+    const auto Color = Scalar(255, 0, 0);
+
+    vector<Rect> faces;
+
+    Mat gray;
     cvtColor(img, gray, COLOR_BGR2GRAY);
+
+    Mat smallImg;
     double fx = 1 / scale;
     resize(gray, smallImg, Size(), fx, fx, INTER_LINEAR_EXACT);
     equalizeHist(smallImg, smallImg);
-    t = (double)getTickCount();
-    cascade.detectMultiScale(smallImg, faces,
-        1.1, 2, 0
-        //|CASCADE_FIND_BIGGEST_OBJECT
-        //|CASCADE_DO_ROUGH_SEARCH
-        | CASCADE_SCALE_IMAGE,
-        Size(30, 30));
-    if (tryflip)
+
+    cascade.detectMultiScale(smallImg, faces, 1.1, 2, CASCADE_SCALE_IMAGE, Size(30, 30));
+
+    for (const auto &face : faces)
     {
-        flip(smallImg, smallImg, 1);
-        cascade.detectMultiScale(smallImg, faces2,
-            1.1, 2, 0
-            //|CASCADE_FIND_BIGGEST_OBJECT
-            //|CASCADE_DO_ROUGH_SEARCH
-            | CASCADE_SCALE_IMAGE,
-            Size(30, 30));
-        for (vector<Rect>::const_iterator r = faces2.begin(); r != faces2.end(); ++r)
-        {
-            faces.push_back(Rect(smallImg.cols - r->x - r->width, r->y, r->width, r->height));
-        }
-    }
-    t = (double)getTickCount() - t;
-    printf("detection time = %g ms\n", t * 1000 / getTickFrequency());
-    for (size_t i = 0; i < faces.size(); i++)
-    {
-        Rect r = faces[i];
         Mat smallImgROI;
         vector<Rect> nestedObjects;
         Point center;
-        Scalar color = colors[i % 8];
+
         int radius;
-        double aspect_ratio = (double)r.width / r.height;
+
+        auto aspect_ratio = (double)face.width / face.height;
         if (0.75 < aspect_ratio && aspect_ratio < 1.3)
         {
-            center.x = cvRound((r.x + r.width*0.5)*scale);
-            center.y = cvRound((r.y + r.height*0.5)*scale);
-            radius = cvRound((r.width + r.height)*0.25*scale);
-            circle(img, center, radius, color, 3, 8, 0);
+            center.x = cvRound((face.x + face.width*0.5)*scale);
+            center.y = cvRound((face.y + face.height*0.5)*scale);
+            radius = cvRound((face.width + face.height)*0.25*scale);
+
+            circle(img, center, radius, Color, 3, 8, 0);
         }
         else
         {
-            rectangle(img, Point(cvRound(r.x*scale), cvRound(r.y*scale)),
-                Point(cvRound((r.x + r.width - 1)*scale), cvRound((r.y + r.height - 1)*scale)),
-                color, 3, 8, 0);
+            rectangle(img, Point(cvRound(face.x*scale), cvRound(face.y*scale)),
+                Point(cvRound((face.x + face.width - 1)*scale), cvRound((face.y + face.height - 1)*scale)),
+                Color, 3, 8, 0);
         }
 
-        if (nestedCascade.empty()) continue;
-        smallImgROI = smallImg(r);
-        nestedCascade.detectMultiScale(smallImgROI, nestedObjects,
-            1.1, 2, 0
-            //|CASCADE_FIND_BIGGEST_OBJECT
-            //|CASCADE_DO_ROUGH_SEARCH
-            //|CASCADE_DO_CANNY_PRUNING
-            | CASCADE_SCALE_IMAGE,
-            Size(30, 30));
-        for (size_t j = 0; j < nestedObjects.size(); j++)
+        smallImgROI = smallImg(face);
+        nestedCascade.detectMultiScale(smallImgROI, nestedObjects, 1.1, 2, CASCADE_SCALE_IMAGE, Size(30, 30));
+        for (const auto &nr : nestedObjects)
         {
-            Rect nr = nestedObjects[j];
-            center.x = cvRound((r.x + nr.x + nr.width*0.5)*scale);
-            center.y = cvRound((r.y + nr.y + nr.height*0.5)*scale);
+            center.x = cvRound((face.x + nr.x + nr.width*0.5)*scale);
+            center.y = cvRound((face.y + nr.y + nr.height*0.5)*scale);
             radius = cvRound((nr.width + nr.height)*0.25*scale);
-            circle(img, center, radius, color, 3, 8, 0);
+
+            circle(img, center, radius, Color, 3, 8, 0);
         }
     }
 
