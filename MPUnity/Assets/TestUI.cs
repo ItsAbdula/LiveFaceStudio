50dbb258fe008679bc6856f2cdbac1ced9c10566
imnhk <koloke1712@gmail.com>
2020-04-15 09:53:10 +0900

    얼굴 파츠(눈) 색 변경 테스트
    결국 텍스처 파일을 수정해야 할 듯

---
+++ MPUnity/Assets/TestUI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUI : MonoBehaviour
+{
+    private int _currentCharacterIndex = 0;
+
+    [SerializeField]
+    private Color _currentColor;
+
+    [SerializeField]
+    private GameObject[] characters;
+    [SerializeField]
+    private GameObject[] leftEyes;
+
+    void Start()
+    {
+        ShowCharacter(_currentCharacterIndex);
+    }
+
+    public void ShowNextCharacter()
+    {
+        _currentCharacterIndex = (_currentCharacterIndex + 1) % characters.Length;
+        ShowCharacter(_currentCharacterIndex);
+    }
+
+    public void SetEyeColor()
+    {
+        if(leftEyes[_currentCharacterIndex] == null)
+        {
+            return;
+        }
+
+        leftEyes[_currentCharacterIndex].GetComponent<Live2D.Cubism.Rendering.CubismRenderer>().Color = _currentColor;
+    }
+
+    private void ShowCharacter(int index)
+    {
+        for (int i = 0; i < characters.Length; i++)
+        {
+            if (i == index)
+            {
+                characters[i].SetActive(true);
+            }
+            else
+            {
+                characters[i].SetActive(false);
+            }
+        }
+    }
+}
