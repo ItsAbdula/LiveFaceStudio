b84ef1ba98774b4bf907e942eede4da211a521c4
imnhk <koloke1712@gmail.com>
2020-04-28 17:34:04 +0900

    TestUI.cs 위치, 이름 변경
    UIManager로 변경하고 색 및 파라미터 변경 기능을 분리할 예정

---
+++ MPUnity/Assets/Scripts/UIManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static int currentCharacterIndex = 4;
+    public static GameObject currentCharacter;
+
+    [SerializeField]
+    private GameObject[] characters;
+    [SerializeField]
+    private GameObject[] leftEyes;
+
+    private void Awake()
+    {
+        ShowCharacter(currentCharacterIndex);
+        currentCharacter = characters[currentCharacterIndex];
+    }
+
+    public void ShowNextCharacter()
+    {
+        currentCharacterIndex = (currentCharacterIndex + 1) % characters.Length;
+        ShowCharacter(currentCharacterIndex);
+    }
+
+    public void SetEyeColor()
+    {
+        // 변경 예정
+    }
+
+    private void ShowCharacter(int index)
+    {
+        for (int i = 0; i < characters.Length; i++)
+        {
+            if (i == index)
+            {
+                characters[i].SetActive(true);
+            }
+            else
+            {
+                characters[i].SetActive(false);
+            }
+        }
+    }
+}
